603b19becadf6b9670a259b4b9a62e2a45657073
Dhrumil Patel <dhrumilp15@gmail.com>
2020-01-09 22:39:55 -0500

    Epic models time

--- bindec.py
+++
@@ -1,3 +0,0 @@
-print(int('1001 1001 1101'.replace(' ', ''),2) - int('1010 0101 0101'.replace(' ', ''),2))
-print(int('1010 0101 0101'.replace(' ', ''),2))
-print(int('0111 1100 1000'.replace(' ', ''),2))
